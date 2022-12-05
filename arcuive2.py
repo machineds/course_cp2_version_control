e24477853a84ce2b56e1f81cd1736f0c3f3159ae
carloscruz01 <ds102turma@gmail.com>
2022-12-05 09:37:01 -0300

    O código criará um arquivo e logo após imprimir uma mensagem do tipo string.

--- arcuive2.py
+++ arcuive2.py
@@ -2,3 +2,6 @@ def criation_archive():
     print('Hello world, i am Blastoff.')
     var = open(mode='x', file="archive2.py")
     open('archirve2')
+def hello():
+    print('Hello world, i am padawan teen. I stay learning scientific computing with python.')
+hello()
